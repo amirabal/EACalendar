7e5d7854e870341b512514e0be6a08ae9aede191
Alex Mirabal <alex.m.141@gmail.com>
2017-05-01 20:04:04 -0400

    new views 	deleted:    AddEventViewController.swift 	new file:   EACalendar/AddEventDelegate.swift 	modified:   EACalendar/Base.lproj/Main.storyboard 	deleted:    EACalendar/CardView.swift 	deleted:    EACalendar/CircularTransition.swift 	new file:   EACalendar/Objc-Bridge-Header.h 	new file:   EACalendar/ViewController.swift 	new file:   EACalendar/EventTableViewCell.swift 	new file:   EACalendar/AddViewController.swift

---
+++ EACalendar/AddEventDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AddEventDelegate.swift
+//  EACalendar
+//
+//  Created by Alex Mirabal on 4/10/17.
+//  Copyright © 2017 alex. All rights reserved.
+//
+
+protocol AddEventDelegate {
+    // MARK: Event Added Delegate
+    func eventDidAdd() 
+}
